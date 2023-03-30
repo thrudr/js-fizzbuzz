866b0230dbad8377a18e7e7907938e4db973d4f5
thrudr <ariel.oliveiraesilva@gmail.com>
2023-03-30 16:54:03 +0200

    square-test

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ let multiplesTreeAndFive = "FizzBuzz";
 for(let i= 1; i < 101; i++) {
     if(i%3==0 && i%5==0){
         console.log(multiplesTreeAndFive);
+        document.getElementById("box-square").innerHTML +=`
+        
+        `;
 
     } else if(i%5==0){
         console.log(multiplesFive);
+        document.getElementById("box-square").innerHTML +=`
+       
+        `;
 
     } else if(i%3==0) {
         console.log(multiplesTree);
+        document.getElementById("box-square").innerHTML +=`
+       
+        `;
 
     } else {
         console.log(i);
+        document.getElementById("box-square").innerHTML +=`
+       
+        `;
     }
 }
+
